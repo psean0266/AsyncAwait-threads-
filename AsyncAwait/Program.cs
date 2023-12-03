6de2bf9b4f890571c431ffb3c1498d0bde73140f
AbdoulayePangnonKOUL <AbdoulayePangnonKOUL@TAL-7G6S793>
2023-12-03 19:41:28 +0100

    first commit

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+
+namespace AsyncAwait
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+
+            Console.WriteLine("Téléchargement en cours");
+            string url = "https://codeavecjonathan.com/res/dummy.txt";
+            var task = httpClient.GetStringAsync(url);
+            task.Wait();
+
+            Console.WriteLine("OK");
+
+            Console.WriteLine(task.Result); 
+
+        }
+    }
+}
